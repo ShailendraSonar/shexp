27b9148c7dd5bcf11157ed0e38f561e16b072ba2
ShailendraSonar <ssonar61@gmail.com>
2024-08-08 17:57:17 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'splitwise-clone',
+  webDir: 'dist'
+};
+
+export default config;
